a5b4f681ff794b7c76bc8833f0f6ecfea0f5ddef
Rokas <rookas.rudzenskas@gmail.com>
2022-04-04 16:21:49 +0300

    Rokas

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,63 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import { useWeb3 } from '@3rdweb/hooks'
+import { useEffect } from 'react'
+import {async} from "rxjs";
+
+const style = {
+    wrapper: ``,
+    walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42] `,
+    button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
+    details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
+}
 
 const Home: NextPage = () => {
-  return (
-    <div className="">
+    const { address, connectWallet } = useWeb3();
+
+    useEffect(() => {
+        if (!address) return
+            ;(async () => {
+            const userDoc = {
+                _type: 'users',
+                _id: address,
+                userName: 'Unnamed',
+                walletAddress: address,
+            }
+
+            const result = await client.createIfNotExists(userDoc)
+
+            welcomeUser(result.userName)
+        })()
+    }, [address])
+
+    return (
+    <div className={style.wrapper}>
       <Head>
         <title>BlockChain Marketplace</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
-        <Hero />
+
+        {address ? (
+            <>
+                <Header />
+                <Hero />
+            </>
+        ) : (
+            <div className={style.walletConnectWrapper}>
+                <button
+                    className={style.button}
+                    onClick={() => connectWallet('injected')}
+                >
+                    Connect Wallet
+                </button>
+                <div className={style.details}>
+                    You need Chrome to be
+                    <br /> able to run this app.
+                </div>
+            </div>
+        )}
     </div>
   )
 }
