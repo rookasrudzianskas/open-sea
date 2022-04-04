14ff96294b838b00ebf4845fc4b4c60bf21be2ab
Rokas <rookas.rudzenskas@gmail.com>
2022-04-04 16:14:33 +0300

    Rokas

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import {ThirdwebWeb3Provider} from '@3rdweb/hooks';
+import { useEffect } from 'react';
+
+const supportedChainIds = [4];
+const connectors = {
+  injected: {},
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
