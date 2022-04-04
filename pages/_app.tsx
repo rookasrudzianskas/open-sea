3e84cb8d3df21863edc02496d72311af38bbf613
Rokas <rookas.rudzenskas@gmail.com>
2022-04-04 16:15:17 +0300

    Rokas

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,12 @@ import type { AppProps } from 'next/app';
 import {ThirdwebWeb3Provider} from '@3rdweb/hooks';
 import { useEffect } from 'react';
 
+
+/**
+ * The chain ID 4 represents the Rinkeby network
+ * The `injected` connector is a web3 connection method used by Metamask
+ */
+
 const supportedChainIds = [4];
 const connectors = {
   injected: {},
