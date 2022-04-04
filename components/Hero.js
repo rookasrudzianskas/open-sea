6ad57982000022910329915a19854c6acbfc0ab4
Rokas <rookas.rudzenskas@gmail.com>
2022-04-04 16:05:36 +0300

    ROkas

--- components/Hero.js
+++ components/Hero.js
@@ -29,12 +29,37 @@ const Hero = () => {
                         <div className={style.description}>
                             OpenSea is the world&apos;s first and largest marketplace for NFTs.
                         </div>
+                        <div className={style.ctaContainer}>
+                            <button className={style.accentedButton}>Explore</button>
+                            <button className={style.button}>Create</button>
+                        </div>
                     </div>
 
+                    <div className={style.cardContainer}>
+                        <img
+                            className="rounded-t-lg"
+                            src="https://lh3.googleusercontent.com/ujepnqpnL0nDQIHsWxlCXzyw4pf01yjz1Jmb4kAQHumJAPrSEj0-e3ABMZlZ1HEpJoqwOcY_kgnuJGzfXbd2Tijri66GXUtfN2MXQA=s550"
+                            alt=""
+                        />
+                        <div className={style.infoContainer}>
+                            <img
+                                className="h-[2.25rem] rounded-full"
+                                src="https://lh3.googleusercontent.com/qQj55gGIWmT1EnMmGQBNUpIaj0qTyg4YZSQ2ymJVvwr_mXXjuFiHJG9d3MRgj5DVgyLa69u8Tq9ijSm_stsph8YmIJlJQ1e7n6xj=s64"
+                                alt=""
+                            />
+                            <div className={style.author}>
+                                <div className={style.name}>Jolly</div>
+                                <a
+                                    className="text-[#1868b7]"
+                                    href="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/2324922113504035910649522729980423429926362207300810036887725141691069366277"
+                                >
+                                    hola-kanola
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
             </div>
-
         </div>
     );
 };
