b5d553fd85b06adb08e6b3edfcdbd130fc263e3b
Rokas <rookas.rudzenskas@gmail.com>
2022-04-04 16:04:14 +0300

    Rokas

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header';
+import Hero from '../components/Hero';
 
 const Home: NextPage = () => {
   return (
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
+        <Hero />
     </div>
   )
 }
