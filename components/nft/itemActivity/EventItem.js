71f1200cf5ec1b70d05f27f78cf30c830f8b5c9f
Rokas <rookas.rudzenskas@gmail.com>
2022-04-05 09:32:21 +0300

    Rokas

--- components/nft/itemActivity/EventItem.js
+++ components/nft/itemActivity/EventItem.js
@@ -12,30 +12,29 @@ const style = {
     accent: `text-[#2081e2]`,
 }
 
-const EventItem = ({event}) => {
+const EventItem = ({ event }) => {
     return (
         <div className={style.eventItem}>
             <div className={`${style.event} flex-[2]`}>
                 <div className={style.eventIcon}>
                     <BsFillCartFill />
                 </div>
-                <div className={style.eventName}>
-                    Sale
-                </div>
-                <div className={`${style.eventPrice} flex-[2]`}>
-                    <img
-                        src="https://storage.opensea.io/files/6f8e2979d428180222796ff4a33ab929.svg"
-                        alt="eth"
-                        className={style.ethLogo}
-                    />
-                    <div className={style.eventPriceValue}>{event.price}</div>
-                </div>
-                <div className={`${style.accent} flex-[3]`}>{event.from}</div>
-                <div className={`${style.accent} flex-[3]`}>{event.to}</div>
-                <div className={`${style.accent} flex-[2]`}>{event.date}</div>
+                <div className={style.eventName}>Sale</div>
             </div>
+            <div className={`${style.eventPrice} flex-[2]`}>
+                <img
+                    src="https://storage.opensea.io/files/6f8e2979d428180222796ff4a33ab929.svg"
+                    alt="eth"
+                    className={style.ethLogo}
+                />
+                <div className={style.eventPriceValue}>{event.price}</div>
+            </div>
+            <div className={`${style.accent} flex-[3]`}>{event.from}</div>
+            <div className={`${style.accent} flex-[3]`}>{event.to}</div>
+            <div className={`${style.accent} flex-[2]`}>{event.date}</div>
         </div>
-    );
-};
+    )
+}
+
 
 export default EventItem;
