45c11f62fa71c7a8ae48852c1b17c0a106cdc8ae
Rokas <rookas.rudzenskas@gmail.com>
2022-04-05 08:51:27 +0300

    Rokas

---
+++ pages/nfts/[nftId].js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from "../../components/Header";
+import { useEffect, useMemo, useState } from 'react'
+import { useWeb3 } from '@3rdweb/hooks'
+import { ThirdwebSDK } from '@3rdweb/sdk'
+import { useRouter } from 'next/router'
+
+const style = {
+    wrapper: `flex flex-col items-center container-lg text-[#e5e8eb]`,
+    container: `container p-6`,
+    topContent: `flex`,
+    nftImgContainer: `flex-1 mr-4`,
+    detailsContainer: `flex-[2] ml-4`,
+}
+
+const Nft = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+};
+
+export default Nft;
