d5748ebf5b2429e299bb69caa8b1ae0b0c3eadfe
Rokas <rookas.rudzenskas@gmail.com>
2022-04-05 09:39:40 +0300

    Rokas

--- components/nft/Purchase.js
+++ components/nft/Purchase.js
@@ -12,7 +12,54 @@ const style = {
 
 const MakeOffer = ({ isListed, selectedNft, listings, marketPlaceModule }) => {
     const [selectedMarketNft, setSelectedMarketNft] = useState()
-    const [enableButton, setEnableButton] = useState(false)
+    const [enableButton, setEnableButton] = useState(false);
+
+    useEffect(() => {
+        if(!listings || isListed === 'false') return;
+        ;(async () => {
+            setSelectedMarketNft(
+                listings.find((marketNft) => marketNft.asset?.id === selectedNft.id)
+            )
+        })()
+    }, [selectedNft, listings, isListed]);
+
+    useEffect(() => {
+        if (!selectedMarketNft || !selectedNft) return
+
+        setEnableButton(true)
+    }, [selectedMarketNft, selectedNft]);
+
+
+    const confirmPurchase = (toastHandler = toast) =>
+        toastHandler.success(`Purchase successful!`, {
+            style: {
+                background: '#04111d',
+                color: '#fff',
+            },
+        });
+
+    const buyItem = async (
+        listingId = selectedMarketNft.id,
+        quantityDesired = 1,
+        module = marketPlaceModule
+    ) => {
+        console.log(listingId, quantityDesired, module, 'rokas')
+        // yo ROKAs lets goooo!!!
+        //yo TOm, ok
+        // sure okay about to run it...
+        // just clicked buy now...
+        // still error
+        // where can i see the contract address of the marketplace module
+        // in [nftId.js]
+        await module
+            .buyoutDirectListing({
+                listingId: listingId,
+                quantityDesired: quantityDesired,
+            })
+            .catch((error) => console.error(error))
+
+        confirmPurchase()
+    }
 
     return (
         <div>
