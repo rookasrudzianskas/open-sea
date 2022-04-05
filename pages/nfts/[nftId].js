9fe1da5b46ab8c60333f59401b7e95fd240df705
Rokas <rookas.rudzenskas@gmail.com>
2022-04-05 08:56:02 +0300

    Rokas

--- pages/nfts/[nftId].js
+++ pages/nfts/[nftId].js
@@ -15,9 +15,23 @@ const style = {
 
 const Nft = () => {
     const {provider} = useWeb3();
-    const [selectedNft, setSelectedNft] = useWeb3();
+    const [selectedNft, setSelectedNft] = useState()
     const [listings, setListings] = useState([]);
     const router = useRouter();
+
+    const nftModule = useMemo(() => {
+        if (!provider) return
+
+        const sdk = new ThirdwebSDK(
+            provider.getSigner(),
+            'https://eth-rinkeby.alchemyapi.io/v2/rclOC_G5gQff7eqOZ-ZuoUpYCzYy2D18'
+        )
+        return sdk.getNFTModule('0xeb38262776CcD2a7CdAD70686719f3F82898Fef0')
+    }, [provider]);
+
+
+
+
 
     return (
         <div>
