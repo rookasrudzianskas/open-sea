40b3412636794bc3f14e4489dfff7f6f94352df8
Rokas <rookas.rudzenskas@gmail.com>
2022-04-05 09:42:40 +0300

    Rokas

--- components/nft/Purchase.js
+++ components/nft/Purchase.js
@@ -62,7 +62,29 @@ const MakeOffer = ({ isListed, selectedNft, listings, marketPlaceModule }) => {
     }
 
     return (
-        <div>
+        <div className="flex h-20 w-full items-center rounded-lg border border-[#151c22] bg-[#303339] px-12">
+            <Toaster position="bottom-left" reverseOrder={false} />
+            {isListed === 'true' ? (
+                <>
+                    <div onClick={() => {
+                        enableButton ? buyItem(selectedMarketNft.id, 1) : null
+                    }} className={`${style.button} bg-[#2081e2] hover:bg-[#42a0ff]`}>
+                        <IoMdWallet className={style.buttonIcon} />
+                        <div className={style.buttonText}>Buy Now</div>
+                    </div>
+                    <div
+                        className={`${style.button} border border-[#151c22]  bg-[#363840] hover:bg-[#4c505c]`}
+                    >
+                        <HiTag className={style.buttonIcon} />
+                        <div className={style.buttonText}>Make Offer</div>
+                    </div>
+                </>
+            ): (
+                <div className={`${style.button} bg-[#2081e2] hover:bg-[#42a0ff]`}>
+                    <IoMdWallet className={style.buttonIcon} />
+                    <div className={style.buttonText}>List Item</div>
+                </div>
+            )}
 
         </div>
     );
